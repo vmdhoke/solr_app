76609fc91db9dd46bcc04fc4c1424b85fed70bad
set <vaibhav.dhoke@mirraw.com>
2016-03-18 19:57:33 +0530

    view created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+  
+  Product.create(name: 'product1',description: 'MyText this is the first product',per_day_rent: 100,rating: 9.99,category: 'Outside',status: 9.99,score: 9.99,location: 'Mumbai',quantity: 9.99)
+  Product.create(name: 'product2',description: 'MyText this is the second product',per_day_rent: 100,rating: 9.99,category: 'Inside',status: 9.99,score: 9.99,location: 'Pune',quantity: 9.99)
+  Product.create(name: 'product3',description: 'MyText this is the third product',per_day_rent: 100,rating: 9.99,category: 'Outside',status: 9.99,score: 9.99,location: 'Mumbai',quantity: 9.99)
+  Product.create(name: 'product4',description: 'MyText this is the fourth product',per_day_rent: 100,rating: 9.99,category: 'Inside',status: 9.99,score: 9.99,location: 'Pune',quantity: 9.99)
+   Product.create(name: 'product5',description: 'MyText this is the fourth product',per_day_rent: 100,rating: 9.99,category: 'Outside',status: 9.99,score: 9.99,location: 'Pune',quantity: 9.99)
